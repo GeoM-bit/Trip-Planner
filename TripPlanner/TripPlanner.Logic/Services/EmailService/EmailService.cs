9260105496c878b4630ee118e610405b60363742
Dragos Boboluta <dragos.boboluta@nagarro.com>
2023-01-25 08:35:49 +0200

    email sender destinatar email update .

--- TripPlanner/TripPlanner.Logic/Services/EmailService/EmailService.cs
+++ TripPlanner/TripPlanner.Logic/Services/EmailService/EmailService.cs
@@ -72,7 +72,7 @@ namespace TripPlanner.Logic.Services.EmailService
         public async void SendEmail(string status,string email) 
         {
             MailAddress from = new MailAddress(_user.Email);
-            MailAddress to = new MailAddress("paul.crasmareanu@nagarro.com");
+            MailAddress to = new MailAddress(email);
             MailMessage message = new MailMessage(from, to);
             
             message.IsBodyHtml = true;
